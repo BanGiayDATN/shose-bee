6ce118cf1f10cc848b65aaa4462bdbe1b7d31338
thangdtph27626 <109157942+thangdtph27626@users.noreply.github.com>
2023-06-29 15:04:24 +0700

    update sidebar

---
+++ FE/src/pages/admin/voucher/MemberOffers.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function MemberOffers() {
+  return (
+    <div className="home">
+      {/* mới sửa */}
+      <SidebarProject /> 
+      <div className="homeContainer">
+        <Navbar />
+        
+      </div>
+    </div>
+  )
+}
+
+export default MemberOffers
