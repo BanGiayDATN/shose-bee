9cb531db725114b76b2a666d1f3781a1333cd7a1
thangdtph27626 <109157942+thangdtph27626@users.noreply.github.com>
2023-07-21 09:36:13 +0700

    clearn import *

--- BE/src/main/java/com/example/shose/server/controller/PaymentsMethodRestController.java
+++ BE/src/main/java/com/example/shose/server/controller/PaymentsMethodRestController.java
@@ -1,11 +1,17 @@
 package com.example.shose.server.controller;
 
 import com.example.shose.server.dto.request.paymentsmethod.CreatePaymentsMethodRequest;
-import com.example.shose.server.repository.PaymentsMethodRepository;
 import com.example.shose.server.service.PaymentsMethodService;
 import com.example.shose.server.util.ResponseObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * @author thangdt
