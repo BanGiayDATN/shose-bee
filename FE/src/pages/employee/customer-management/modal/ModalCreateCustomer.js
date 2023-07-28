2947c51f3db0308a170336fa497ca80086a630c6
Ngô Vũ Ngọc Hào <ngovungochao14112003@gmail.com>
2023-07-28 22:04:50 +0700

    Giao diện Customer mới

--- FE/src/pages/employee/customer-management/modal/ModalCreateCustomer.js
+++ FE/src/pages/employee/customer-management/modal/ModalCreateCustomer.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Modal, Input, Select, Button, Form } from "antd";
+import React, { useState } from "react";
+import { Modal, Input, Select, Button, Form, Row, Col, Upload } from "antd";
 import { useAppDispatch } from "../../../../app/hook";
 import { toast } from "react-toastify";
 import { CustomerApi } from "../../../../api/employee/account/customer.api";
@@ -9,6 +9,8 @@ import "../style-account.css";
 import { faKaaba } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import moment from "moment";
+import { PlusOutlined, StarFilled, StarOutlined } from "@ant-design/icons";
+// import QrReader from "react-qr-reader";
 
 const { Option } = Select;
 
@@ -16,6 +18,30 @@ const ModalCreateCustomer = ({ visible, onCancel }) => {
   const [form] = Form.useForm();
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </div>
+  );
+
+  // const [qrData, setQrData] = useState("");
+
+  // const handleScan = (data) => {
+  //   if (data) {
+  //     setQrData(data);
+  //   }
+  // };
+
+  // const handleError = (error) => {
+  //   console.error("QR Scan Error:", error);
+  // };
 
   const handleOk = () => {
     form
@@ -76,113 +102,243 @@ const ModalCreateCustomer = ({ visible, onCancel }) => {
         <FontAwesomeIcon icon={faKaaba} style={{ fontSize: "26px" }} />
         <span style={{ marginLeft: "10px" }}>Quản lý tài khoản khách hàng</span>
       </div>
-      <div className="filter">
-        <div
-          className="content-wrapper"
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+      <div
+        className="content-wrapper"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <span
+          style={{ fontSize: "25px", fontWeight: "bold", marginTop: "10px" }}
         >
-          <span
-            style={{ fontSize: "25px", fontWeight: "bold", marginTop: "10px" }}
-          >
-            TẠO TÀI KHOẢN
-          </span>
-        </div>
-        <div className="title_add">
-          <Form form={form} layout="vertical">
-            <Form.Item
-              label="Tên khách hàng"
-              name="fullName"
-              rules={[
-                { required: true, message: "Vui lòng nhập tên khách hàng" },
-                { max: 30, message: "Tên khách hàng tối đa 30 ký tự" },
-              ]}
-            >
-              <Input placeholder="Tên khách hàng" />
-            </Form.Item>
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[
-                { required: true, message: "Vui lòng nhập email" },
-                { max: 20, message: "Email tối đa 20 ký tự" },
-                {
-                  pattern: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
-                  message: "Email không đúng định dạng",
-                },
-              ]}
-            >
-              <Input placeholder="Email" />
-            </Form.Item>
-            <Form.Item
-              label="Mật khẩu"
-              name="password"
-              rules={[
-                { required: true, message: "Vui lòng nhập mật khẩu" },
-                { min: 8, message: "Mật khẩu phải 8 ký tự" },
-              ]}
-            >
-              <Input placeholder="Mật khẩu" />
-            </Form.Item>
-
-            <Form.Item
-              label="Số điện thoại"
-              name="phoneNumber"
-              rules={[
-                { required: true, message: "Vui lòng nhập số điện thoại" },
-                {
-                  pattern: /^0\d{9}$/,
-                  message:
-                    "Số điện thoại phải bắt đầu từ số 0 và gồm 10 chữ số",
-                },
-              ]}
-            >
-              <Input placeholder="Số điện thoại" />
-            </Form.Item>
-            <Form.Item
-              label="Ngày sinh"
-              name="dateOfBirth"
-              rules={[
-                { required: true, message: "Vui lòng chọn ngày sinh" },
-                { validator: validateAge },
-              ]}
-            >
-              <Input type="date" />
-            </Form.Item>
-            <Form.Item
-              label="Điểm"
-              name="points"
-              rules={[{ required: true, message: "Vui lòng nhập điểm" }]}
-            >
-              <Input type="number" placeholder="Điểm" />
-            </Form.Item>
-            <Form.Item label="Trạng thái" name="status">
-              <Select
-                placeholder="Vui lòng chọn trạng thái"
-                defaultValue="DANG_SU_DUNG"
-                disabled
-              >
-                <Option value="DANG_SU_DUNG">Kích hoạt</Option>
-                <Option value="KHONG_SU_DUNG">Ngừng kích hoạt</Option>
-              </Select>
-            </Form.Item>
-            <Button
-              key="submit"
-              type="primary"
-              onClick={handleOk}
-              style={{ marginRight: "10px" }}
-            >
-              Thêm
-            </Button>
-            <Button key="cancel" onClick={handleCancel}>
-              Hủy
-            </Button>
-          </Form>
-        </div>
+          THÊM KHÁCH HÀNG
+        </span>
       </div>
+      <Row gutter={[24, 8]}>
+        <Col className="filter" span={6}>
+          {/* <Link to="/promotion-management">
+            <LeftOutlined /> Quay lại
+          </Link> */}
+          <h1
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "10px",
+              marginBottom: "30px",
+            }}
+          >
+            Ảnh đại diện
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Upload
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              listType="picture-circle"
+            >
+              {uploadButton}
+            </Upload>
+          </div>
+        </Col>
+
+        <Col className="filter" span={17} style={{ marginLeft: "20px" }}>
+          {/* <div className="filter"> */}
+          <Form form={form} layout="vertical">
+            <Row gutter={[24, 8]}>
+              <Col span={10}>
+                <div className="title_add">
+                  <Form.Item
+                    label="Tên khách hàng"
+                    name="fullName"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Vui lòng nhập tên khách hàng",
+                      },
+                      { max: 30, message: "Tên khách hàng tối đa 30 ký tự" },
+                    ]}
+                  >
+                    <Input
+                      className="input-item"
+                      placeholder="Tên khách hàng"
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                      { required: true, message: "Vui lòng nhập email" },
+                      { max: 20, message: "Email tối đa 20 ký tự" },
+                      {
+                        pattern:
+                          /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+                        message: "Email không đúng định dạng",
+                      },
+                    ]}
+                  >
+                    <Input className="input-item" placeholder="Email" />
+                  </Form.Item>
+                  <Form.Item
+                    label="Mật khẩu"
+                    name="password"
+                    rules={[
+                      { required: true, message: "Vui lòng nhập mật khẩu" },
+                      { min: 8, message: "Mật khẩu phải 8 ký tự" },
+                    ]}
+                  >
+                    <Input className="input-item" placeholder="Mật khẩu" />
+                  </Form.Item>
+                  <Form.Item
+                    label="Tỉnh/Thành phố"
+                    name="province"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Vui lòng chọn Tỉnh/Thành phố",
+                      },
+                    ]}
+                  >
+                    <Select defaultValue="">
+                      {/* <Select defaultValue="" onChange={handleProvinceChange}> */}
+                      <Option value="">--Chọn Tỉnh/Thành phố--</Option>
+                    </Select>
+                  </Form.Item>
+
+                  <Form.Item
+                    label="Xã/Phường"
+                    name="werd"
+                    rules={[
+                      { required: true, message: "Vui lòng chọn Xã/Phường" },
+                    ]}
+                  >
+                    <Select defaultValue="">
+                      <Option value="">--Chọn Xã/Phường--</Option>
+                    </Select>
+                  </Form.Item>
+
+                  <Form.Item style={{ marginTop: "40px" }} name="userId" hidden>
+                    <Input disabled />
+                  </Form.Item>
+                  <Form.Item
+                    style={{ marginTop: "40px" }}
+                    name="toDistrictId"
+                    hidden
+                  >
+                    <Input disabled />
+                  </Form.Item>
+                  <Form.Item label="Trạng thái" name="status">
+                    <Select
+                      placeholder="Vui lòng chọn trạng thái"
+                      defaultValue="DANG_SU_DUNG"
+                      disabled
+                    >
+                      <Option value="DANG_SU_DUNG">Kích hoạt</Option>
+                      <Option value="KHONG_SU_DUNG">Ngừng kích hoạt</Option>
+                    </Select>
+                  </Form.Item>
+
+                  <Button
+                    key="submit"
+                    type="primary"
+                    onClick={handleOk}
+                    style={{ marginRight: "10px" }}
+                  >
+                    Thêm
+                  </Button>
+                  <Button key="cancel" onClick={handleCancel}>
+                    Hủy
+                  </Button>
+                </div>
+              </Col>
+              <Col span={10} style={{ marginLeft: "40px", marginTop: "16px" }}>
+                <Form.Item
+                  label="Số điện thoại"
+                  name="phoneNumber"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Vui lòng nhập số điện thoại",
+                    },
+                    {
+                      pattern: /^0\d{9}$/,
+                      message:
+                        "Số điện thoại phải bắt đầu từ số 0 và gồm 10 chữ số",
+                    },
+                  ]}
+                >
+                  <Input className="input-item" placeholder="Số điện thoại" />
+                </Form.Item>
+                <Form.Item
+                  label="Ngày sinh"
+                  name="dateOfBirth"
+                  rules={[
+                    { required: true, message: "Vui lòng chọn ngày sinh" },
+                    { validator: validateAge },
+                  ]}
+                >
+                  <Input className="input-item" type="date" />
+                </Form.Item>
+                <Form.Item
+                  label="Điểm"
+                  name="points"
+                  rules={[{ required: true, message: "Vui lòng nhập điểm" }]}
+                >
+                  <Input
+                    className="input-item"
+                    type="number"
+                    placeholder="Điểm"
+                  />
+                </Form.Item>
+                <Form.Item
+                  label="Quận/Huyện"
+                  name="district"
+                  rules={[
+                    { required: true, message: "Vui lòng chọn Quận/Huyện" },
+                  ]}
+                >
+                  {/* <Select defaultValue=" " onChange={handleDistrictChange}> */}
+                  <Select defaultValue=" ">
+                    <Option value=" ">--Chọn Quận/Huyện--</Option>
+                  </Select>
+                </Form.Item>
+
+                <Form.Item
+                  label="Số nhà/Ngõ/Đường"
+                  name="line"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Vui lòng nhập số nhà/ngõ/đường",
+                    },
+                  ]}
+                >
+                  <Input
+                    className="input-item"
+                    placeholder="Số nhà/Ngõ/Đường"
+                  />
+                </Form.Item>
+                {/* <div>
+                  <QrReader
+                    delay={300}
+                    onError={handleError}
+                    onScan={handleScan}
+                    style={{ width: "100%" }}
+                  />
+                  <p>Scanned Data: {qrData}</p>
+                </div> */}
+              </Col>
+            </Row>
+          </Form>
+          {/* </div> */}
+        </Col>
+      </Row>
     </div>
   );
 };
