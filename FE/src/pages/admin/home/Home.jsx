bbae6afec1a34d2070f52c6127595281fd5448b5
vinhnv01 <109213729+vinhnv01@users.noreply.github.com>
2023-09-16 00:53:35 +0700

    no message

--- FE/src/pages/admin/home/Home.jsx
+++ FE/src/pages/admin/home/Home.jsx
@@ -16,14 +16,14 @@ const Home = () => {
           <Widget type="user" />
           <Widget type="order" />
           <Widget type="earning" />
-          <Widget type="balance" />
+          <Widget type="product" />
         </div>
         <div className="charts">
           <Featured />
           <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
         </div>
         <div className="listContainer">
-          <div className="listTitle">Latest Transactions</div>
+          <div className="listTitle">Giao dịch mới nhất</div>
           <Table />
         </div>
       </div>
