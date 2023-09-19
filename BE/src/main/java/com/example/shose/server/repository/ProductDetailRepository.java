5bc07d1e5e00df2d7aebcc92166361e69732341a
vinhnv01 <109213729+vinhnv01@users.noreply.github.com>
2023-09-19 10:06:46 +0700

    no message

--- BE/src/main/java/com/example/shose/server/repository/ProductDetailRepository.java
+++ BE/src/main/java/com/example/shose/server/repository/ProductDetailRepository.java
@@ -106,7 +106,7 @@ public interface ProductDetailRepository extends JpaRepository<ProductDetail, St
                     LEFT JOIN image i ON max_images.max_image_id = i.id
                     JOIN color co on co.id = detail.id_color
                     JOIN size s on s.id = detail.id_size
-           where p.id = '3e730115-70b6-42f6-b4db-a76ad3598425'
+           where p.id = :id
            group by detail.id
             """, nativeQuery = true)
     List<GetProductDetailByProduct> getByIdProduct(@Param("id") String id);
@@ -353,16 +353,16 @@ public interface ProductDetailRepository extends JpaRepository<ProductDetail, St
 
     ProductDetailDTOResponse getOneById(@Param("id") String id);
     @Query(value = """
-SELECT
-    s.name as nameSize,
-    p.id as idProduct,
-    REPLACE(c.code, '#','%23') as codeColor
-FROM product_detail detail
-join product p on detail.id_product = p.id
-join size s on detail.id_size = s.id
-join color c on detail.id_color = c.id
-where p.id = :idProduct and c.code = :codeColor
-""",nativeQuery = true)
+                SELECT
+                    s.name as nameSize,
+                    p.id as idProduct,
+                    REPLACE(c.code, '#','%23') as codeColor
+                FROM product_detail detail
+                join product p on detail.id_product = p.id
+                join size s on detail.id_size = s.id
+                join color c on detail.id_color = c.id
+                where p.id = :idProduct and c.code = :codeColor
+            """,nativeQuery = true)
     List<ListSizeOfItemCart> listSizeByProductAndColor(@Param("idProduct") String idProduct, @Param("codeColor") String codeColor);
 
 }
