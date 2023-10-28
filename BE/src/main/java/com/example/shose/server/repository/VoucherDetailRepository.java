7036ae748754af183e958bbe0b084e9bc7a4abcf
thangdtph27626 <109157942+thangdtph27626@users.noreply.github.com>
2023-10-28 14:12:46 +0700

    Update VoucherDetailRepository.java

--- BE/src/main/java/com/example/shose/server/repository/VoucherDetailRepository.java
+++ BE/src/main/java/com/example/shose/server/repository/VoucherDetailRepository.java
@@ -2,12 +2,13 @@ package com.example.shose.server.repository;
 
 import com.example.shose.server.dto.response.voucher.VoucherDetailCustomAtCountry;
 import com.example.shose.server.entity.VoucherDetail;
+import com.example.shose.server.entity.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 /**
  * @author Nguyễn Vinh
  */
