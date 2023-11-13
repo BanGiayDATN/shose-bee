194cb03d741fae421af94752dd4d94dfa395aeba
vinhnv01 <109213729+vinhnv01@users.noreply.github.com>
2023-11-13 15:10:12 +0700

    set up quản lý đơn hàng của user

---
+++ FE/src/pages/customer/account/purchase/tabscontent/TabAllBill.jsx
@@ -0,0 +1,20 @@
+
+export default function TabAllBill() {
+
+  const data ={
+    name: "okkkk",
+    image:"",
+    quantity: 3,
+    price : 1000,
+    sumBillPrice : 3000,
+  }
+
+  return (
+    <>
+      <h1>TabAllBill</h1>
+      <div>
+
+      </div>
+    </>
+  );
+}
---
+++ FE/src/pages/customer/account/purchase/tabscontent/TabHoanThanh.jsx
@@ -0,0 +1,7 @@
+export default function TabHoanThanh() {
+  return (
+    <>
+      <h1>TabHoanThanh</h1>
+    </>
+  );
+}
