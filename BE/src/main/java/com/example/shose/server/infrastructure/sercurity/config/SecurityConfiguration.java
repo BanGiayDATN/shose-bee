685c7af607742a0e6678b60de7ccd811064affc5
vinhnv01 <109213729+vinhnv01@users.noreply.github.com>
2023-11-28 03:45:05 +0700

    fix Cross-Origin

--- BE/src/main/java/com/example/shose/server/infrastructure/sercurity/config/SecurityConfiguration.java
+++ BE/src/main/java/com/example/shose/server/infrastructure/sercurity/config/SecurityConfiguration.java
@@ -83,8 +83,6 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH"));
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedOrigin("http://103.56.161.210:3000");
         configuration.addAllowedOrigin("http://komf.id.vn:3000");
         configuration.addAllowedHeader("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
