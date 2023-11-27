69af3ef562545f027793fbb7e960bb691fe103f6
thangdtph27626 <109157942+thangdtph27626@users.noreply.github.com>
2023-11-27 14:47:50 +0700

    Update DetailBill.js

--- FE/src/pages/employee/bill-management/DetailBill.js
+++ FE/src/pages/employee/bill-management/DetailBill.js
@@ -892,7 +892,7 @@ function DetailBill() {
                           marginLeft: "20px",
                         }}
                       >
-                        {listStatus[statusPresent + 1].name}
+                        {billHistory.some((item) => item.status === "DA_THANH_TOAN") && bill.statusBill === "VAN_CHUYEN" ? "Thành công" : listStatus[statusPresent + 1].name}
                       </Button>
                     ) : (
                       <div></div>
