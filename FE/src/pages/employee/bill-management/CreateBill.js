c0f7dd4e196690fc89ed0ba2c8deff8fd2366933
thangdtph27626 <109157942+thangdtph27626@users.noreply.github.com>
2023-12-07 14:24:28 +0700

    Update CreateBill.js

--- FE/src/pages/employee/bill-management/CreateBill.js
+++ FE/src/pages/employee/bill-management/CreateBill.js
@@ -1134,7 +1134,7 @@ function CreateBill({
     setCodeVoucher(record.code + " - " + record.name);
     setIsModalVoucherOpen(false);
   };
-
+const changeQuanTiTy = useSelector((state) => state.bill.bill.change);
   useEffect(() => {
     // Tính tổng giá tiền dựa trên số lượng sản phẩm và giá của từng sản phẩm
     const newTotalPrice = products.reduce(
@@ -1162,7 +1162,7 @@ function CreateBill({
     setCodeVoucher(record.code + " - " + record.name);
     setIsModalVoucherOpen(false);
     }
-  }, [products]);
+  }, [products, changeQuanTiTy]);
   
   const [voucher, setVoucher] = useState({
     idVoucher: "",
